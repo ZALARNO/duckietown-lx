1b3a12f5cd0f703b3c0ebbe2778a3829c5dc569c
ZALARNO <rossa3@bfh.ch>
2024-10-16 11:39:13 +0000

    Braitenberg exercise done

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,11 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+   
+    for j in range(10) :
+        for i in range(350-30*j) :
+           res[i+30*j+130, 320:(440+ i -7*j):] = -j/20 - 0.25
+    
     # ---
     return res
 
@@ -17,6 +20,18 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+
+    for j in range(10) :
+        for i in range(350-30*j) :
+           res[i+30*j+130, (200-i + 7*j):320] = -j/20 - 0.25
     # ---
     return res
+
+
+#    for j in range(10) :
+#        for i in range(330-20*j) :
+#           res[i+20*j+150, 320:(340+i):] = -j/20 - 0.5
+#
+#    for j in range(10) :
+#        for i in range(330-20*j) :
+#           res[i+20*j+150, (300-i):320] = -j/20 - 0.25
